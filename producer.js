4c200ced7489c6257a55753a3b3f7fb89ded0bda
Shreyash Jain <shreyash.jain.eee15@itbhu.ac.in>
2021-05-05 23:27:16 +0530

    kafka test

---
+++ producer.js
@@ -0,0 +1,43 @@
+const readline = require('readline');
+const kafka = require('kafka-node'),
+    Producer = kafka.Producer,
+    client = new kafka.KafkaClient(),
+    producer = new Producer(client);
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+producer.on('ready', () => {
+    const waitForUserInput = () => {
+        rl.question('Command: ', (answer) => {
+            if (answer == 'exit') {
+                rl.close();
+            } else if (isNaN(answer)) {
+                waitForUserInput();
+            } else {
+                console.log("hello");
+                let payload = [
+                    {
+                       
+                        topic: 'quickstart-events',
+                        messages: answer,
+                    }
+                ];
+                producer.send(payload, (err, data) => {
+                    waitForUserInput();
+                });
+            }
+        });
+        rl.on('SIGINT', () => {
+            rl.close();
+        });
+    }
+    waitForUserInput();
+});
+
+producer.on('error', (err) => {
+    console.log(err);
+});
+
+
